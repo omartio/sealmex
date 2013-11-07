8c37df873a503fefc3afd068dbefe3c04187fe1a
omartio <mvlukyanov@gmail.com>
2013-11-08 01:22:36 +0400

    Добавил переход по страницам сайта и начал переключение чз пагинацию

--- js/script.js
+++ js/script.js
@@ -1,10 +1,28 @@
 $(document).ready(function(){
-
 	$('#main').load('main.html');
+	var currPage = $('#info'); 	// открытая страница
+	var index = 0; 				// номер пагинации
 	
-	$('.nav a').click(function()
-	{
-		$('#main').load($(this).attr('data-target'));
+	$('.nav a').click(function(){
+		currPage.parent().removeClass('active');
+		openPage($(this).attr('data-target'));
+		currPage = $(this);
+		currPage.parent().addClass('active');
+	});
+	// НЕ РАБОТАЕТ!!!!!!
+	$('.pagination a').click(function(){
+		alert('fff');
+		var i = $('.pagination a').index(this);
+		$('.pag').css('display', 'none');
+		$('.pag:eq('+i+')').css('display', 'block');
 	});
 });
 
+function openPage(url){
+	$('#main').fadeOut(function(){
+		$('#main').load(url, function(){
+			$('#main').fadeIn();
+		});
+	});
+	
+}
