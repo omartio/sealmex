4437adf0872feeda1de3d79c125e54e1fecfd258
omartio <mvlukyanov@gmail.com>
2013-11-11 23:34:28 +0400

    убрал ненужное

--- js/script.js
+++ js/script.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
 	$('#main').load('main.html');
 	var currPage = $('#info'); 	// открытая страница
-	var index = 0; 				// номер пагинации
 	
 	$('.nav a').click(function(){
 		currPage.parent().removeClass('active');
 		openPage($(this).attr('data-target'));
 		currPage = $(this);
 		currPage.parent().addClass('active');
-	});
-	// НЕ РАБОТАЕТ!!!!!!
-	$('.pagination a').click(function(){
-		alert('fff');
-		var i = $('.pagination a').index(this);
-		$('.pag').css('display', 'none');
-		$('.pag:eq('+i+')').css('display', 'block');
 	});
 });
 
@@ -25,4 +17,6 @@ function openPage(url){
 		});
 	});
 	
-}
+};
+
+
